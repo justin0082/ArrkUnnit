0e87229e8b4f59157bcf9cd2b0160ff9fa337f3b
justin0082 <woojinchoi0082@gmail.com>
2025-10-18 08:19:18 +0900

    지랄

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public int direction;
+    public float wind_speed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
